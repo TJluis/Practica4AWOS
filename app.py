d44196952ebb4b18b52a74fd2d70a1ded1ed198f
TJluis <tsluisj@gmail.com>
2026-02-23 10:31:33 -0600

    Update app.py
    Json

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import flask, render_template, request, jsonify, make_response, session
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/producto')
+def productos():
+    import mysql.connector
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="concencionario"
+    )
+
+    mycursor = mydb.cursor()
+    mycursor.execute("SELECT * FROM concencionaria")
+    myresult = mycursor.fetchall()
+    return make_response(jsonify(myresult))
+
