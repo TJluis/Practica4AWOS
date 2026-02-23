6377d6dbea784e4d9d8be622374584e35b3b12e6
TJluis <tsluisj@gmail.com>
2026-02-23 10:42:01 -0600

    Update app.py
    se corrigio el Flask

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import flask, render_template, request, jsonify, make_response, session
+from flask import Flask, render_template, request, jsonify, make_response, session
 app = Flask(__name__)
 
 @app.route('/producto')
